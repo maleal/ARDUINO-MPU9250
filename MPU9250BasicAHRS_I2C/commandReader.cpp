02758cdceab535beba68d5e668bb15dd20d5c8ca
maleal <mariolealfuentes@gmail.com>
2018-02-01 21:09:04 -0300

    Agrego archivos iniciales para implementar la lectura de radio comandos

---
+++ MPU9250BasicAHRS_I2C/commandReader.cpp
@@ -0,0 +1,5 @@
+#include "commandReader.h"
+//==============================================================================
+//====== Set of useful function to read commands/values form RF transmiter
+//====== by Mario, Leal Fuentes.
+//==============================================================================
---
+++ MPU9250BasicAHRS_I2C/commandReader.h
@@ -0,0 +1,8 @@
+#ifndef _COMMANDREADER_H_
+#define _COMMANDREADER_H_
+
+class {
+}
+
+
+#endif//_COMMANDREADER_H_
